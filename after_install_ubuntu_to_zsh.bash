c4fea811ceeb0964355598f9bc45bec063f83a40
James Chiang <jameslce@yahoo.com.tw>
2018-10-08 18:36:36 +0800

    first commit

---
+++ after_install_ubuntu_to_zsh.bash
@@ -0,0 +1,21 @@
+
+#!/bin/bash
+
+sudo apt-get remove -y libreoffice-common unity-webapps-common thunderbird totem rhythmbox empathy brasero simple-scan gnome-mahjongg
+sudo apt-get remove -y aisleriot gnome-mines cheese transmission-common gnome-orca webbrowser-app gnome-sudoku onboard deja-dup
+
+sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y
+
+sudo apt-get install -y zsh git powerline fonts-powerline vim
+chsh -s /bin/zsh
+
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+echo "" >> ~/.zshrc
+echo "setopt no_nomatch" >> ~/.zshrc
+echo "" >> ~/.zshrc
+
+sudo init 6
+
